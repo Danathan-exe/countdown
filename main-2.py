9ee35533a7b3d8503e4f30d3ce4b07d0f2ed0414
Danathan-exe <56946882+Danathan-exe@users.noreply.github.com>
2022-10-09 20:39:29 -0700

    Add files via upload

---
+++ main-2.py
@@ -0,0 +1,17 @@
+# CSC1019-680-Fall22
+# DDEBONIS
+# 10/6/2022
+#creates the function
+def blastoff(c):
+  while c > 0:
+    print(c) #prints the value being displayed
+    c = c -1 #subtracts by 1 until hits zero.
+    if c == 0: #inserts a if value for when it reaches 0 to say blastoff
+      print("Blastoff!")
+      break
+    #relays and prints the function
+print(blastoff(100))
+    
+
+  
+
